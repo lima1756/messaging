17dcce360563d03204fd0c3ec7273ff4cb92c82f
Iván Morett <luisivanmorett@gmail.com>
2019-11-10 13:57:45 -0600

    demo controller

--- src/Server.ts
+++ src/Server.ts
@@ -1,12 +1,36 @@
+import * as path from 'path';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as controllers from './controllers';
 import { Server } from '@overnightjs/core';
 import { Logger } from '@overnightjs/logger';
+
 
 class MessagingServer extends Server {
 
     private readonly SERVER_START_MSG = 'Demo server started on port: ';
+    private readonly DEV_MSG = 'Express Server is running in development mode. ' + 
+        'No front-end content is being served.';
 
     constructor() {
-        super();
+        super(true);
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: true}));
+        this.setupControllers();
+        if (process.env.NODE_ENV !== 'production'){
+            this.app.get('*', (req, res) => res.send(this.DEV_MSG));
+        }
+    }
+
+    private setupControllers(): void {
+        const ctlrInstances =  [];
+        for (const name in controllers) {
+            if (controllers.hasOwnProperty(name)) {
+                let Controller = (controllers as any)[name];
+                ctlrInstances.push(new Controller());
+            }
+        }
+        super.addControllers(ctlrInstances);
     }
 
     public start(port: number): void {
