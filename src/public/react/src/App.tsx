788c9e35ec7f889dd757ab4a8f828609be14a7ca
Iván Morett <luisivanmorett@gmail.com>
2019-11-11 16:17:11 -0600

    Message bar design

--- src/public/react/src/App.tsx
+++ src/public/react/src/App.tsx
@@ -1,35 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-async function callExpress() {
-  try {
-    let response = await fetch('/api/say-hello/LIMA')
-      .then(res => res.json().then(json=> alert('Hi this is a response from the backend: ' + json.message)));
-    
-  } catch (err) {
-    alert(err);
-  }
-}
+import { Icon, Button, SideNav, SideNavItem, Textarea } from 'react-materialize';
 
 const App: React.FC = () => {
-  callExpress();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <header>
+        <SideNav fixed={true}>
+          <SideNavItem href="#!icon" icon="cloud">
+            First Link  With Icon
+          </SideNavItem>
+        </SideNav>
       </header>
+      <main>
+        <div className='container'>
+          <div className='row'>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </div>
+          
+        </div>
+      </main>
+      <footer className="message-footer">
+        <div className="container">
+          <div className="row">
+          <div className="col s11">
+              <Textarea label="Write Your Message" className="messagebox input-field" s={12}/>
+            </div>
+            <div className="col s1">
+              <Button type="submit" waves="light" className="input-field">
+                <Icon center>
+                send
+                </Icon>
+              </Button>
+            </div>
+          </div>
+        </div>
+          
+      </footer>
     </div>
   );
 }
--- src/public/react/src/index.tsx
+++ src/public/react/src/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'; 
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
