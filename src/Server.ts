24570ac86716f2b58d6cebc66ede6644c0a93ae8
Iván Morett <luisivanmorett@gmail.com>
2019-11-19 14:23:04 -0600

    logging

--- src/Server.ts
+++ src/Server.ts
@@ -14,25 +14,33 @@ class MessagingServer extends Server {
     private io: SocketIOController;
     private port: number | string;
     private static readonly PORT: number = 3001;
-    private static readonly SERVER_START_MSG: string = 'Demo server started on port: ';
+    private static readonly SERVER_START_MSG: string = 'Server started on port: ';
     private static readonly DEV_MSG: string = 'Express Server is running in development mode. ' + 
         'No front-end content is being served.';
     private static readonly PATH: string = path.join(__dirname, 'public', 'messaging-react');
 
     constructor() {
         super(true);
+        Logger.Info("Loading");
         this.app.use(cors);
+        Logger.Info("CORS ready");
         this.app.use(bodyParser.json());
+        Logger.Info("json ready");
         this.app.use(bodyParser.urlencoded({extended: true}));
+        Logger.Info("url ready");
         this.app.use(express.static(MessagingServer.PATH));
+        Logger.Info("static ready");
         this.port = process.env.PORT || MessagingServer.PORT;
         this.io = new SocketIOController(socketIO.listen(3002));
+        Logger.Info("socket io");
         this.setupControllers();
+        Logger.Info("controllers");
         if (process.env.NODE_ENV !== 'production'){
             this.app.get('*', (req, res) => res.send(MessagingServer.DEV_MSG));
         }
         else{
             this.app.get('*', (req, res) => res.sendFile(path.join(MessagingServer.PATH, 'index.html' )));
+            Logger.Info("served");
         }
     }
 
